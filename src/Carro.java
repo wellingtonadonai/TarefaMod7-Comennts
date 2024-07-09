c4bc3239da2b61792632b46b3a6a8769dafe7c67
wellington <wellington.h.s.oliveira@hotmail.com>
2024-07-09 10:54:28 -0300

    Tarefa do modulo 7 da Ebac Sobre comentarios

---
+++ src/Carro.java
@@ -0,0 +1,26 @@
+public class Carro {
+
+    private String cor;
+    private int ano;
+
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
+/**
+ * criei a classe Carro e defini suas atribuições e métodos
+ *
+ */
